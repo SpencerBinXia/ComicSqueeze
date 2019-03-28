94ac83f967d8e4b36d3abb202a25792f5146c4e5
Spencer X <spencerbinxia@gmail.com>
2019-03-27 20:21:39 -0400

    Initial commit

---
+++ src/main/java/com/comicsqueeze/comicsqueeze/ComicsqueezeApplication.java
@@ -0,0 +1,13 @@
+package com.comicsqueeze.comicsqueeze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicsqueezeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComicsqueezeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/comicsqueeze/comicsqueeze/ComicsqueezeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.comicsqueeze.comicsqueeze;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComicsqueezeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
