c28559ba56d40137a4292140016b5114c26ef81d
Spencer X <spencerbinxia@gmail.com>
2019-04-03 19:53:59 -0400

    Controller for Profile Path Variables - broken CSS?

--- src/main/java/com/comicsqueeze/comicsqueeze/controller/OtherUserProfileController.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/controller/OtherUserProfileController.java
@@ -3,15 +3,17 @@ package com.comicsqueeze.comicsqueeze.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/otherprof")
+@RequestMapping("/viewprofile/{profileID}")
 public class OtherUserProfileController {
 
     @GetMapping
-    public String home(Model model)
+    public String displayProfile(@PathVariable("profileID") String profileID, Model model)
     {
+        System.out.println(profileID);
         return "OtherUserProfile";
     }
 
