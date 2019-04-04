59343c62e5818dbe9b6a8a36fb6d19ab63d05000
Varun Batra <varun.batra@stonybrook.edu>
2019-04-03 22:40:51 -0400

    registration

--- src/main/java/com/comicsqueeze/comicsqueeze/ComicsqueezeApplication.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/ComicsqueezeApplication.java
@@ -1,12 +1,42 @@
 package com.comicsqueeze.comicsqueeze;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @SpringBootApplication
 public class ComicsqueezeApplication {
 
-    public static void main(String[] args) { SpringApplication.run(ComicsqueezeApplication.class, args);
+    public static void main(String[] args) throws FirebaseAuthException {
+        SpringApplication.run(ComicsqueezeApplication.class, args);
+        FileInputStream serviceAccount = null;
+        try {
+            serviceAccount = new FileInputStream("C:/Users/Varun/cse308/comicsqueeze/ComicSqueeze/src/main/resources/comicsqueeze-firebase-adminsdk-cgyos-49457802ed.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://comicsqueeze.firebaseio.com/")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 
 }
--- src/main/java/com/comicsqueeze/comicsqueeze/controller/RegisterController.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/controller/RegisterController.java
@@ -1,18 +1,64 @@
 package com.comicsqueeze.comicsqueeze.controller;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
-@RequestMapping("/register")
 public class RegisterController {
 
-    @GetMapping
-    public String home(Model model)
-    {
+    @RequestMapping("/register")
+    public String home(Model model) {
         return "Register";
     }
 
+    @RequestMapping("/registerUser")
+    public String registerUser(Model m,@RequestParam String Username, @RequestParam String Password, @RequestParam String Email, @RequestParam String First, @RequestParam String Last) {
+        try {
+            UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                    .setEmail(Email)
+                    .setEmailVerified(false)
+                    .setPassword(Password)
+                    .setDisplayName(Username);
+
+            UserRecord userRecord = null;
+            userRecord = FirebaseAuth.getInstance().createUser(request);
+            System.out.println("Successfully created new user: " + userRecord.getUid());
+            return "CurrentUserProfile";
+        } catch (FirebaseAuthException e) {
+            String message;
+            if (e.getMessage().equals("User management service responded with an error")){
+                message= "Email already in use";
+            }
+            else{
+                message = e.getMessage();
+            }
+            m.addAttribute("err2",message);
+
+            m.addAttribute(("err3"),"registration unsuccessful make sure all fields are valid");
+            return "Register";
+        } catch (IllegalArgumentException e) {
+            String message;
+            if (e.getMessage().equals("User management service responded with an error")){
+                message= "Email already in use";
+            }
+            else{
+                message = e.getMessage();
+            }
+            m.addAttribute("err2",message);
+            m.addAttribute(("err3"),"registration unsuccessful make sure all fields are valid");
+            return "Register";
+        }
+
+
+
+
+    }
 }
