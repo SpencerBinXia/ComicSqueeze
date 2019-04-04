2bd5d5bea421b32ffcdc485a51d20cf2bc51fba7
usman <usman.ansari@stonybrook.edu>
2019-04-03 21:54:02 -0400

    Added loginRegister Service and added register/findbyUser method to Profile Repo

--- src/main/java/com/comicsqueeze/comicsqueeze/object/User.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/object/User.java
@@ -3,20 +3,14 @@ import java.util.ArrayList;
 
 public class User {
     private String username;
-    private String password;
-    private String email;
     private boolean active;
     private boolean admin;
 
     public String getUsername() {return username;}
-    public String getPassword() {return password;}
-    public String getEmail() {return email;}
     public Boolean getActiveStatus() {return active;}
     public Boolean getAdminStatus() {return admin;}
 
     public void setUsername(String name){this.username = name;}
-    public void setPassword(String pass){this.password = pass;}
-    public void setEmail(String email) {this.email = email;}
     public void setActiveStatus(boolean active) {this.active = active;}
     public void setAdminStatus(boolean admin) {this.admin = admin;}
 }
--- src/main/java/com/comicsqueeze/comicsqueeze/repository/ProfileRepo.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/repository/ProfileRepo.java
@@ -2,8 +2,10 @@ package com.comicsqueeze.comicsqueeze.repository;
 
 import com.comicsqueeze.comicsqueeze.object.User;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
@@ -11,8 +13,36 @@ import java.sql.SQLException;
 
 @Repository
 public class ProfileRepo {
+    @Autowired
+    JdbcTemplate jdbc;
 
+    //Might need to change the table name, depending on the table name for user
+    public User findByName(String username)
+    {
+        String findUser ="SELECT * FROM user WHERE user_name='" + username + "';";
+        User tempuser = new User();
+        try
+        {
+            jdbc.queryForObject(findUser, new RowMapper<User>() {
+                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    tempuser.setUsername(rs.getString("user_name"));
+                    return tempuser;
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+        return tempuser;
+    }
 
+    //Might need to change the table name, depending on the table name for user
+    public void regUser(User newUser)
+    {
+        jdbc.update("INSERT INTO user(user_name)" + "VALUES(?,?)", newUser.getUsername());
+
+    }
 
 
 }
