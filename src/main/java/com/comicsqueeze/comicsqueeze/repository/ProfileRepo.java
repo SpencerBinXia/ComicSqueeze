9dde4b4b394502368aec395aac6491141f63ef9b
usman <usman.ansari@stonybrook.edu>
2019-04-03 21:12:13 -0400

    Added User Object and Profile Repo

---
+++ src/main/java/com/comicsqueeze/comicsqueeze/repository/ProfileRepo.java
@@ -0,0 +1,18 @@
+package com.comicsqueeze.comicsqueeze.repository;
+
+import com.comicsqueeze.comicsqueeze.object.User;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+public class ProfileRepo {
+
+
+
+
+}
