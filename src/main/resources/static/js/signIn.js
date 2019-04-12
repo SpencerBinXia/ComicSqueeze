61ae02f5c46f9b8f58b4150e03adb93c8b9fffaa
Varun Batra <varun.batra@stonybrook.edu>
2019-04-11 20:31:13 -0400

    commenting updateProfilePic

--- src/main/resources/static/js/signIn.js
+++ src/main/resources/static/js/signIn.js
@@ -113,19 +113,24 @@ function redirectToProfile(user) {
     });}
 
 function updateProfilePic() {
+    // get a new file reader
     var reader  = new FileReader();
+    //get the file picked
     var file    = document.querySelector('input[type=file]').files[0]; //sames as here
     var user = firebase.auth().currentUser;
 
     reader.onloadend = function () {
-
+        // when done loading file as url send to firebase
         user.updateProfile({
-            photoURL: reader.result.toSource
+            //Reader.result is the img url
+            // if we store this in our db and get it back when coming to profle it should work
+            photoURL: reader.result
         }).then(function() {
             console.log("success");
         }).catch(function(error) {
             console.log(error);
         });
+        // set the img container src to url of img file
         document.getElementById("profPic").src = reader.result;
         console.log(user.photoURL);
         console.log(reader.result.toSource);
