1af0534e63c237fb4955e32cd250373580ce4871
Spencer X <spencerbinxia@gmail.com>
2019-04-15 19:59:51 -0400

    Navigate to unique Series pages

--- src/main/java/com/comicsqueeze/comicsqueeze/controller/SeriesController.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/controller/SeriesController.java
@@ -3,16 +3,20 @@ package com.comicsqueeze.comicsqueeze.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/series")
+@RequestMapping("/series/{profileID}/{seriesTitle}")
 public class SeriesController {
     @GetMapping
-    public String home(Model model, HttpSession session)
+    public String home(@PathVariable("profileID") String profileID, @PathVariable("seriesTitle") String seriesTitle, Model model, HttpSession session)
     {
+        System.out.println("seriescontroller");
+        model.addAttribute("profileID", profileID);
+        model.addAttribute("seriesTitle", seriesTitle);
         return "SeriesPage";
     }
 }
