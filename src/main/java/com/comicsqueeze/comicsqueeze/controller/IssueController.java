55f6dc134d7ddd68c537a2fe33381e7041dafcaa
Varun Batra <varun.batra@stonybrook.edu>
2019-04-15 21:21:17 -0400

    adding profile id back to issue controller

--- src/main/java/com/comicsqueeze/comicsqueeze/controller/IssueController.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/controller/IssueController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
@@ -16,7 +17,7 @@ public class IssueController {
     @Autowired
     private loginRegisterService service;
     @GetMapping
-    public String home(Model model, HttpSession session)
+    public String displayProfile(@PathVariable("profileID") String profileID, Model model, HttpSession session)
     {
         Member curMember = service.findMember((String)session.getAttribute("username"));
         model.addAttribute("curMember", curMember);
