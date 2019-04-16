b0b70d1a21936d588d7196d4341ff83ea606ada0
Spencer X <spencerbinxia@gmail.com>
2019-04-15 20:39:55 -0400

    Navigate to unique Issue pages

--- src/main/java/com/comicsqueeze/comicsqueeze/controller/IssueController.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/controller/IssueController.java
@@ -3,17 +3,21 @@ package com.comicsqueeze.comicsqueeze.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/issue")
+@RequestMapping("/series/{profileID}/{seriesTitle}/{issueTitle}")
 public class IssueController {
 
     @GetMapping
-    public String home(Model model, HttpSession session)
+    public String home(@PathVariable("profileID") String profileID, @PathVariable("seriesTitle") String seriesTitle, @PathVariable("issueTitle") String issueTitle,  Model model, HttpSession session)
     {
+        model.addAttribute("profileID", profileID);
+        model.addAttribute("seriesTitle", seriesTitle);
+        model.addAttribute("issueTitle", issueTitle);
         return "IssuePage";
     }
 
