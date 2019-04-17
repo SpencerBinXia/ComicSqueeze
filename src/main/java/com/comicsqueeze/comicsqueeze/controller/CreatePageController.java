30e7c67683bba007e2e7ff62e5c41864e20960b6
trimartin12 <trimartin12@users.noreply.github.com>
2019-04-16 21:54:53 -0400

    Add Review modal, series and issues options now dependant on who is viewing the page. Added full navbar to CreatePage

--- src/main/java/com/comicsqueeze/comicsqueeze/controller/CreatePageController.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/controller/CreatePageController.java
@@ -2,6 +2,7 @@ package com.comicsqueeze.comicsqueeze.controller;
 
 import com.comicsqueeze.comicsqueeze.object.Member;
 import com.comicsqueeze.comicsqueeze.service.loginRegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +15,14 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/createpage")
 public class CreatePageController {
 
+    @Autowired
     private loginRegisterService service;
+
     @GetMapping
-    public String home(Model model, @RequestParam(value ="userName", defaultValue = "USERNAME") String userName, HttpSession session)
+    public String home(Model model, HttpSession session)
     {
         Member curMember = service.findMember((String)session.getAttribute("username"));
         model.addAttribute("curMember", curMember);
-        model.addAttribute("userName",userName);
         return "CreatePage";
     }
 
