9766cd76b608cadb87d5334c88797314f81093df
Varun Batra <varun.batra@stonybrook.edu>
2019-04-16 21:44:47 -0400

    Saving state of the user and saving page based on values in the state

--- src/main/java/com/comicsqueeze/comicsqueeze/controller/IssueController.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/controller/IssueController.java
@@ -1,6 +1,8 @@
 package com.comicsqueeze.comicsqueeze.controller;
 
+import com.comicsqueeze.comicsqueeze.object.Issue;
 import com.comicsqueeze.comicsqueeze.object.Member;
+import com.comicsqueeze.comicsqueeze.service.ComicIssueService;
 import com.comicsqueeze.comicsqueeze.service.loginRegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +18,8 @@ import javax.servlet.http.HttpSession;
 public class IssueController {
     @Autowired
     private loginRegisterService service;
+    @Autowired
+    private ComicIssueService issueService;
     @GetMapping
     public String displayProfile(@PathVariable("profileID") String profileID, @PathVariable("seriesTitle") String seriesTitle, @PathVariable("issueTitle") String issueTitle, Model model, HttpSession session)
     {
@@ -25,7 +29,13 @@ public class IssueController {
         model.addAttribute("seriesTitle", seriesTitle);
         model.addAttribute("issueTitle", issueTitle);
         Member member = (Member) session.getAttribute("curMember");
-        member.setCurrentIssue(issueTitle);
+        /*GENERATE MOCK data for now*/
+        Issue newIssue = new Issue();
+        newIssue.setUsername(member.getUsername());
+        newIssue.setTitle(issueTitle);
+        //Issue issue = issueService.findIssueByTitle(member,seriesTitle,issueTitle);
+        /* END OF MOCK DATA */
+        member.setCurrentIssue(newIssue);
         return "IssuePage";
     }
 
--- src/main/java/com/comicsqueeze/comicsqueeze/service/ComicSeriesService.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/service/ComicSeriesService.java
@@ -4,11 +4,16 @@ import com.comicsqueeze.comicsqueeze.object.Member;
 import com.comicsqueeze.comicsqueeze.object.Series;
 import com.comicsqueeze.comicsqueeze.repository.SeriesRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ComicSeriesService {
     @Autowired
     private SeriesRepo seriesRepo;
     public Series findSeriesByTitle(Member member, String seriesTitle){
         return seriesRepo.findBySeriesName(member, seriesTitle);
+    }
+    public void createSeries(Series newSeries){
+        seriesRepo.createSeries(newSeries);
     }
 }
