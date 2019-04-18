68e02641fdcb9968b06c5e087c37cd9bbd57464e
Varun Batra <varun.batra@stonybrook.edu>
2019-04-18 00:53:35 -0400

    adding pages to db

--- src/main/java/com/comicsqueeze/comicsqueeze/controller/CreatePageController.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/controller/CreatePageController.java
@@ -1,6 +1,8 @@
 package com.comicsqueeze.comicsqueeze.controller;
 
 import com.comicsqueeze.comicsqueeze.object.Member;
+import com.comicsqueeze.comicsqueeze.object.Page;
+import com.comicsqueeze.comicsqueeze.service.ComicPageService;
 import com.comicsqueeze.comicsqueeze.service.loginRegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,8 @@ public class CreatePageController {
 
     @Autowired
     private loginRegisterService service;
+    @Autowired
+    private ComicPageService comicPageService;
     @RequestMapping("/createpage")
     public String home(Model model, HttpSession session)
     {
@@ -26,6 +30,15 @@ public class CreatePageController {
     @RequestMapping("/pageDB")
     public String addPageToDB(Model model, HttpSession session, @RequestParam("username") String username, @RequestParam("seriesTitle") String seriesTitle, @RequestParam("issueTitle") String issueTitle, @RequestParam("pageNumber") String pageNumber)
     {
+        Page page = new Page();
+        page.setUsername(username);
+        page.setPagenumber(Integer.valueOf(pageNumber));
+        page.setIssue(issueTitle);
+        page.setImgurl("blah");
+        page.setPublished(false);
+        page.setSeries(seriesTitle);
+        page.setVotes(0);
+        comicPageService.createPage(page);
         Member curMember = service.findMember((String)session.getAttribute("username"));
         model.addAttribute("curMember", curMember);
         return "IssuePage";
