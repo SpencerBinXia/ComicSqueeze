0015a27d06c6e4009fe2800351834b1af2df8592
Spencer X <spencerbinxia@gmail.com>
2019-04-17 23:49:29 -0400

    Viewing other people's series on profile implemented

--- src/main/java/com/comicsqueeze/comicsqueeze/controller/OtherUserProfileController.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/controller/OtherUserProfileController.java
@@ -1,7 +1,9 @@
 package com.comicsqueeze.comicsqueeze.controller;
 
 import com.comicsqueeze.comicsqueeze.object.Member;
+import com.comicsqueeze.comicsqueeze.object.Series;
 import com.comicsqueeze.comicsqueeze.service.loginRegisterService;
+import com.comicsqueeze.comicsqueeze.service.ComicSeriesService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/viewprofile/{profileID}")
@@ -19,11 +22,19 @@ public class OtherUserProfileController {
     @Autowired
     private loginRegisterService service;
 
+    @Autowired
+    private ComicSeriesService comicService;
     @GetMapping
     public String displayProfile(@PathVariable("profileID") String profileID, Model model, HttpSession session)
     {
         Member curMember = service.findMember((String)session.getAttribute("username"));
+        System.out.println(profileID);
+        Member displayMember = service.findMember(profileID);
         model.addAttribute("curMember", curMember);
+        ArrayList<Series> seriesArrayList = comicService.queryAllSeries(displayMember);
+        displayMember.setSeriesArrayList(seriesArrayList);
+        System.out.println(displayMember.getUsername());
+        model.addAttribute("displayMember", displayMember);
         if (profileID.equals(session.getAttribute("username")))
         {
             return "redirect:/yourprofile";
