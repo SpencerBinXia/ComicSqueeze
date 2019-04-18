d37588a153387910601f277168e818d76444305a
usman <usman.ansari@stonybrook.edu>
2019-04-18 00:33:05 -0400

    Allowed for comic issues to be dynamically loaded

--- src/main/java/com/comicsqueeze/comicsqueeze/repository/IssueRepo.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/repository/IssueRepo.java
@@ -2,6 +2,7 @@ package com.comicsqueeze.comicsqueeze.repository;
 
 import com.comicsqueeze.comicsqueeze.object.Issue;
 import com.comicsqueeze.comicsqueeze.object.Member;
+import com.comicsqueeze.comicsqueeze.object.Series;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Repository;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class IssueRepo {
@@ -44,6 +48,23 @@ public class IssueRepo {
     public void createIssue(Issue newIssue){
         jdbc.update("INSERT INTO \"Issue\"(title,series,description,pagecount,timestamp,username)" + "VALUES(?,?,?,?,?,?)", newIssue.getTitle(),newIssue.getSeries(),
                 newIssue.getDescription(),newIssue.getPagecount(),newIssue.getTimestamp(),newIssue.getUsername());
+    }
+
+    public ArrayList<Issue> queryAllIssuesFromASeries(Member member, Series series) {
+        String findIssue = "SELECT * FROM \"Issue\" WHERE username ='" + member.getUsername() + "'AND series='" + series.getTitle() + "';";
+        List<Map<String, Object>> rows = jdbc.queryForList(findIssue);
+        ArrayList<Issue> issues = new ArrayList<>();
+        for (Map rs : rows) {
+            Issue tempIssue = new Issue();
+            tempIssue.setTitle((String)rs.get("seriestitle"));
+            tempIssue.setDescription((String)rs.get("description"));
+            tempIssue.setUsername((String)rs.get("username"));
+            tempIssue.setPagecount((Integer)rs.get("pagecount"));
+            tempIssue.setTimestamp((LocalDateTime)rs.get("time_stamp"));
+            tempIssue.setSeries((String)rs.get("issue"));
+            issues.add(tempIssue);
+        }
+        return issues;
     }
 }
 
