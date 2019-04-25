55fb9ffcf954719f2186e0281bcef3e0f48eb66e
trimartin12 <trimartin12@users.noreply.github.com>
2019-04-24 21:34:19 -0400

    edit tags one by one is no bueno

--- src/main/resources/static/js/ownSeries.js
+++ src/main/resources/static/js/ownSeries.js
@@ -48,64 +48,71 @@ function resetIssueForm() {
 }
 
 function resetEditSeriesForm() {
-    var buttons = document.getElementById("editTagsForm");
-    while (buttons.firstChild) {
-        buttons.removeChild(buttons.firstChild);
-    }
-    console.log("RESET");
-    newTagList.length = 0; // resets the tag list since the form is closed.
+    document.getElementById("editSeriesForm").reset();
+    // var buttons = document.getElementById("editTagsForm");
+    // while (buttons.firstChild) {
+    //     buttons.removeChild(buttons.firstChild);
+    // }
+    // console.log("RESET");
+    // newTagList.length = 0; // resets the tag list since the form is closed.
 }
 
-function populateEditSeries() {
-    var tags = document.getElementById("curTags").innerText;
-    var tagsArray = tags.split(', ');
-    console.log(tagsArray);
-    var form = document.getElementById("editTagsForm");
-    for (var i = 0; i < tagsArray.length; i++){
-        var tagButton = document.createElement("button");
-        newTagList.push(tagsArray[i]);
-        tagButton.id = 'oldTag' + i;
-        tagButton.innerText = tagsArray[i];
-        tagButton.classList.add("tagBtn");
-        tagButton.addEventListener('click', function(){
-            deleteEditTag(tagButton);
-        });
-        form.appendChild(tagButton);
-    }
-}
+// function populateEditSeries() {
+//     var tags = document.getElementById("curTags").innerText;
+//     var tagsArray = tags.split(', ');
+//     console.log(tagsArray);
+//     var form = document.getElementById("editTagsForm");
+//     for (var i = 0; i < tagsArray.length; i++){
+//         var tagButton = document.createElement("button");
+//         newTagList.push(tagsArray[i]);
+//         tagButton.id = 'oldTag' + i;
+//         tagButton.innerText = tagsArray[i];
+//         tagButton.classList.add("tagBtn");
+//         tagButton.addEventListener('click', function(){
+//             deleteEditTag(tagButton);
+//         });
+//         form.appendChild(tagButton);
+//     }
+// }
 
-function deleteEditTag(tag) {
-    // occurs when clicking a newly created tag in the create series popup.
-    var tagText = tag.innerText;
-    console.log("delete tag: " + tagText);
-    for( var i = 0; i < newTagList.length; i++){
-        if ( newTagList[i] == tagText) {
-            newTagList.splice(i, 1);
-            i--;
-        }
-    }
-    console.log("tags list: " +newTagList);
-    var btn = document.getElementById(tag.id);
-    console.log(tag.id);
-    // document.getElementById("editTagsForm").remove(btn);
-    btn.remove();
-}
-function addEditTag(tag) {
-    // occurs when clicking the add tag button after inputting the tag name.
-    console.log("add tag: " +tag);
-    document.getElementById("editTagField").value = "";
-    if(newTagList.includes(tag)){
-        console.log("tags list: " +newTagList);
-        return 0;
-    } else {
-        newTagList.push(tag);
-        console.log("tags list: " +newTagList);
-        return 1;
-    }
-}
+// function deleteEditTag(tag) {
+//     // occurs when clicking a newly created tag in the create series popup.
+//     var tagText = tag.innerText;
+//     console.log("delete tag: " + tagText);
+//     for( var i = 0; i < newTagList.length; i++){
+//         if ( newTagList[i] == tagText) {
+//             newTagList.splice(i, 1);
+//             i--;
+//         }
+//     }
+//     console.log("tags list: " +newTagList);
+//     var btn = document.getElementById(tag.id);
+//     console.log(tag.id);
+//     // document.getElementById("editTagsForm").remove(btn);
+//     btn.remove();
+// }
+// function addEditTag(tag) {
+//     // occurs when clicking the add tag button after inputting the tag name.
+//     console.log("add tag: " +tag);
+//     document.getElementById("editTagField").value = "";
+//     if(newTagList.includes(tag)){
+//         console.log("tags list: " +newTagList);
+//         return 0;
+//     } else {
+//         newTagList.push(tag);
+//         console.log("tags list: " +newTagList);
+//         return 1;
+//     }
+// }
 function editSeries(){
     var descVal = $('#seriesDescField').val();
-    var tagListString = newTagList.join(", "); // creates comma separated string of tags
+    console.log("Desc:" + descVal);
+    // var tagListString = newTagList.join(", "); // creates comma separated string of tags
+    var tagsVal = $('#seriesTagsField').val();
+    console.log("Tags:" + tagsVal);
+
+    $('#descID').text(descVal);
+    $('#curTags').text(tagsVal);
 
     //var edittedSeries = {username: null, collaborative: false, creators: null, description: descVal, rating: 0, title: titleVal, tags: tagListString, timestamp: '2011-12-03T10:15:30', views: 0, weekly: false, flag: false};
     //Check if there were any changes even made
