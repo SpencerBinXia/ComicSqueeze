2e52e3e7a7b3475253c1980f2e8308cae9a544a6
Varun Batra <varun.batra@stonybrook.edu>
2019-05-01 15:17:21 -0400

    Creating repo method to query for every contribution made on the current weekly comic issue amde by an user site- wide

--- src/main/java/com/comicsqueeze/comicsqueeze/repository/WeeklyContributionRepo.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/repository/WeeklyContributionRepo.java
@@ -1,6 +1,7 @@
 package com.comicsqueeze.comicsqueeze.repository;
 
 import com.comicsqueeze.comicsqueeze.object.Issue;
+import com.comicsqueeze.comicsqueeze.object.Page;
 import com.comicsqueeze.comicsqueeze.object.Series;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,6 +11,10 @@ import org.springframework.stereotype.Repository;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 @Repository
 public class WeeklyContributionRepo {
@@ -24,6 +29,9 @@ public class WeeklyContributionRepo {
     /*
         This method adds a contributor to the list of contributors for this issue of the
         weekly comic, if the users is already int the lsit it wont add them again
+         I use the issue object to store the users in because I dont know how to just
+        return one string with a query for some reasone it has to be stored
+        in an object
      */
     public void addContributor(String issueTitle, String username) {
         String getUsers = "SELECT users FROM \"WeeklyComic\" WHERE issuetitle='"+issueTitle+"';";
@@ -52,5 +60,57 @@ public class WeeklyContributionRepo {
             String updateWeekly = "UPDATE \"WeeklyComic\" SET USERS= '" +username + ",';";
             jdbc.update(updateWeekly);
         }
+    }
+    /*
+        This method queries and returns all the contributions made by users site-wide
+        for the currently weekly comic issue
+     */
+    public ArrayList<Page> queryAllContributions(String issueTitle) throws Exception {
+      ArrayList<Page> contributions = new ArrayList<>();
+      // get all the contributors for this issue
+      String contributorsList = getContibutors(issueTitle);
+      //map for each contributor to the pages they created
+      HashMap<String, LinkedList<Integer>> contributorToPages = new HashMap<>();
+      // split them up (contributorList)
+      String [] contributors = contributorsList.split(",");
+      // now for each contributor find there pages on this issue
+      for(String contributor:contributors){
+            findPagesOfContributor(contributor,contributorToPages);
+      }
+      // now put those pages in the list and return it
+        for (String contributor: contributorToPages.keySet()){
+            for(int pageNum: contributorToPages.get(contributor)){
+                //putPageInList(contributor,pageNum,contributions);
+            }
+        }
+
+
+      return contributions;
+
+    }
+
+    private void findPagesOfContributor(String contributor, HashMap<String, LinkedList<Integer>> contributorToPages) {
+
+    }
+
+    /*
+        This method returns the useres of this weekly issue
+        I use the issue object to store the users in because I dont know how to just
+        return one string with a query for some reasone it has to be stored
+        in an object
+     */
+    public String getContibutors(String issueTitle) throws Exception {
+            String getUsers = "SELECT users FROM \"WeeklyComic\" WHERE issuetitle='"+issueTitle+"';";
+            Issue issue = new Issue();
+        jdbc.queryForObject(getUsers, new RowMapper<Issue>() {
+            public Issue mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+                issue.setTitle(rs.getString("users"));
+                return issue;
+            }
+        });
+        return issue.getTitle();
+
+
     }
 }
