c963d507f6dcdc866fc77e343b13ebca442772cb
trimartin12 <trimartin12@users.noreply.github.com>
2019-05-01 18:13:23 -0400

    vote on page dummy front end WIP

---
+++ src/main/resources/static/js/frontPage.js
@@ -0,0 +1,11 @@
+$(".horizontal_slick").slick({
+    dots: true,
+    arrows: true,
+    vertical: false,
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    // rows: 2,
+    // slidesPerRow: 4,
+    verticalSwiping: false,
+});
