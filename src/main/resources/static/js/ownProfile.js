20eb757d5d60af69819ea1f5500c24cc20b1f239
trimartin12 <trimartin12@users.noreply.github.com>
2019-05-17 15:42:28 -0400

    Notifications modal revamping done

--- src/main/resources/static/js/ownProfile.js
+++ src/main/resources/static/js/ownProfile.js
@@ -135,6 +135,21 @@ window.onclick =function (event) {
         modal.style.display = "none";
     }
 }
+// NOTIFICATIONS MODAL REWORK
+function notificationsClicked() {
+    var modal = document.getElementById('notifications');
+    modal.style.display = "block";
+}
+function closeNotifications() {
+    var modal = document.getElementById('notifications');
+    modal.style.display = "none";
+}
+window.onclick =function (event) {
+    var modal = document.getElementById('notifications');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
