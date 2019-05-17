76f66ebb3d890374bdc6fe172b9fd4bf76cae638
usman <usman.ansari@stonybrook.edu>
2019-05-17 19:31:18 -0400

    trying to pull

--- src/main/java/com/comicsqueeze/comicsqueeze/controller/SearchController.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/controller/SearchController.java
@@ -2,6 +2,7 @@ package com.comicsqueeze.comicsqueeze.controller;
 
 import com.comicsqueeze.comicsqueeze.object.Member;
 import com.comicsqueeze.comicsqueeze.object.Series;
+import com.comicsqueeze.comicsqueeze.repository.RateReviewRepo;
 import com.comicsqueeze.comicsqueeze.service.loginRegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.*;
 
 @Controller
 @RequestMapping("/search")
@@ -18,6 +20,9 @@ public class SearchController {
 
     @Autowired
     private loginRegisterService service;
+
+    @Autowired
+    private RateReviewRepo reviewRepo;
 
     @GetMapping
     public String home(Model model, HttpSession session)
@@ -29,7 +34,26 @@ public class SearchController {
             ArrayList<Series> ser = (ArrayList<Series>) session.getAttribute("searchResults");
             model.addAttribute("theResult", ser);
             session.setAttribute("searchResults", null);
-            System.out.println(ser.get(0));
+
+            Map<Series,Double> seriesLow = sortByRatingLow(ser);
+            System.out.println("After Sorting:");
+            Set set2 = seriesLow.entrySet();
+            Iterator iterator2 = set2.iterator();
+            while(iterator2.hasNext()) {
+                Map.Entry me2 = (Map.Entry)iterator2.next();
+                System.out.print(me2.getKey() + ": ");
+                System.out.println(me2.getValue());
+            }
+            model.addAttribute("seriesSortedLow", seriesLow);
+
+
+
+
+
+
+
+            //ArrayList<Series> seriesLow = sortByRatingLow(ser);
+            //System.out.println(ser.get(0));
         }
         else if(session.getAttribute("searchMemberResults") != null){
             ArrayList<Member> mem = (ArrayList<Member>) session.getAttribute("searchMemberResults");
@@ -38,6 +62,37 @@ public class SearchController {
         }
         return "Search";
     }
+
+    private HashMap<Series,Double> sortByRatingLow(ArrayList<Series> series){
+        HashMap<Series,Double> seriesHigh = new HashMap();
+        for(int i =0; i < series.size(); i++){
+            seriesHigh.put(series.get(i),reviewRepo.queryAverageReview(series.get(i).getTitle(), series.get(i).getUsername()));
+        }
+        return sortByValues(seriesHigh);
+    }
+
+    private static HashMap sortByValues(HashMap map) {
+        List list = new LinkedList(map.entrySet());
+        // Defined Custom Comparator here
+        Collections.sort(list, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o1)).getValue())
+                        .compareTo(((Map.Entry) (o2)).getValue());
+            }
+        });
+
+        // Here I am copying the sorted list in HashMap
+        // using LinkedHashMap to preserve the insertion order
+        HashMap sortedHashMap = new LinkedHashMap();
+        for (Iterator it = list.iterator(); it.hasNext();) {
+            Map.Entry entry = (Map.Entry) it.next();
+            sortedHashMap.put(entry.getKey(), entry.getValue());
+        }
+        return sortedHashMap;
+    }
+
+
+
 
 
 }
