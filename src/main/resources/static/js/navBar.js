3a91ba73de93998bdebc202f3ea3aeee42288f20
trimartin12 <trimartin12@users.noreply.github.com>
2019-05-17 17:41:09 -0400

    Sign In modal revamp

--- src/main/resources/static/js/navBar.js
+++ src/main/resources/static/js/navBar.js
@@ -13,3 +13,18 @@ window.onclick =function (event) {
         modal.style.display = "none";
     }
 }
+// LOGIN MODAL REWORK
+function loginClicked() {
+    var modal = document.getElementById('sign-In');
+    modal.style.display = "block";
+}
+function closeLogin() {
+    var modal = document.getElementById('sign-In');
+    modal.style.display = "none";
+}
+window.onclick =function (event) {
+    var modal = document.getElementById('sign-In');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
