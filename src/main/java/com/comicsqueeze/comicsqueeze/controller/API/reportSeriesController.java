063b686d730af9d797b96d9dbc6f133c73ac81ef
usman <usman.ansari@stonybrook.edu>
2019-05-21 18:46:57 -0400

    just to pull

--- src/main/java/com/comicsqueeze/comicsqueeze/controller/API/reportSeriesController.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/controller/API/reportSeriesController.java
@@ -1,5 +1,6 @@
 package com.comicsqueeze.comicsqueeze.controller.API;
 
+import com.comicsqueeze.comicsqueeze.object.Member;
 import com.comicsqueeze.comicsqueeze.object.Series;
 import com.comicsqueeze.comicsqueeze.repository.SeriesRepo;
 import com.comicsqueeze.comicsqueeze.service.ComicSeriesService;
@@ -11,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class reportSeriesController {
@@ -33,6 +38,19 @@ public class reportSeriesController {
     @RequestMapping(value = "/notifyGroup", method = RequestMethod.GET)
     public void notifyGroup(Model model, @RequestParam(value = "group")String collabGroup,@RequestParam(value="seriesCreator") String seriesCreator,
                               @RequestParam(value = "seriesTitle") String seriesTitle){
+
+
+        Member tempMember = new Member();
+        tempMember.setUsername(seriesCreator);
+        ArrayList<Series> newGroupSeries = seriesService.queryforGroupSeries(tempMember);
+        List<String> splits = Arrays.asList(collabGroup.split(","));
+        Boolean read = false;
+        for(int i =0; i<splits.size(); i++){
+            //notificationService.storeNotification(seriesCreator, "","group",splits.get(i),read, false, seriesTitle);
+        }
+
+
+
 
     }
 
