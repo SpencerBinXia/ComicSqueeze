0bb7e43bc4fb296bb62983462e76dfebcf903f7e
Spencer X <spencerbinxia@gmail.com>
2019-05-20 21:13:27 -0400

    Delete Series fixed (subs and ratings are also deleted)

--- src/main/java/com/comicsqueeze/comicsqueeze/controller/API/deleteSeriesController.java
+++ src/main/java/com/comicsqueeze/comicsqueeze/controller/API/deleteSeriesController.java
@@ -1,17 +1,13 @@
 package com.comicsqueeze.comicsqueeze.controller.API;
 
 import com.comicsqueeze.comicsqueeze.object.Member;
-import com.comicsqueeze.comicsqueeze.service.ComicIssueService;
-import com.comicsqueeze.comicsqueeze.service.ComicPageService;
-import com.comicsqueeze.comicsqueeze.service.ComicSeriesService;
+import com.comicsqueeze.comicsqueeze.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.comicsqueeze.comicsqueeze.service.loginRegisterService;
 
 import javax.servlet.http.HttpSession;
 
@@ -30,11 +26,19 @@ public class deleteSeriesController {
     @Autowired
     private ComicSeriesService seriesService;
 
+    @Autowired
+    private SubscriptionService subService;
+
+    @Autowired
+    private RateReviewService rateService;
+
     @RequestMapping(value ="/deleteSeries", method = RequestMethod.GET)
-    public String deleteIssue(Model model, HttpSession session)
+    public String deleteSeries(Model model, HttpSession session)
     {
         Member member = (Member) session.getAttribute("curMember");
         pageService.deleteSeriesPages(member.getCurrentSeries().getTitle(), member.getUsername());
+        subService.removeSubsFromSeries(member.getCurrentSeries().getTitle(), member.getCurrentSeries().getUsername());
+        rateService.deleteRatings(member.getCurrentSeries().getTitle(), member.getCurrentSeries().getUsername());
         issueService.deleteIssues(member.getCurrentSeries().getTitle(), member.getUsername());
         seriesService.deleteSeries(member.getCurrentSeries().getTitle(), member.getUsername());
         return "redirect:/yourprofile";
